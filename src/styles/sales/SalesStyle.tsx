d459febab27c4986b13aaa270d10dae9199567a1
eunjin40 <dev.eunjin40@gmail.com>
2024-05-24 15:38:35 +0900

    feat : SalesPage 퍼블리싱

--- src/styles/sales/SalesStyle.tsx
+++ src/styles/sales/SalesStyle.tsx
@@ -10,9 +10,44 @@ export const Salesstyle = styled.div`
     font-size: 24px;
     font-weight: 700;
   }
-  p{
+  p {
     position: absolute;
     top: 140px;
     font-size: 12px;
+  }
+`;
+
+export const SalesTableHeader = styled.div`
+  width: 970px;
+  margin: 0 auto 20px;
+  display: flex;
+  justify-content: end;
+  span {
+    color: #777;
+    font-size: 18px;
+    font-weight: 400;
+    width: 180px;
+    text-align: center;
+  }
+`;
+
+export const SalesTable = styled.div`
+  display: flex;
+  width: 970px;
+  margin: 0 auto;
+  span {
+    color: #000;
+    font-size: 18px;
+    font-weight: 400;
+    margin: 8px 0px;
+    width: 180px;
+    text-align: center;
+  }
+  span:nth-of-type(1) {
+    width: 50px;
+  }
+  span:nth-of-type(2) {
+    width: 380px;
+    text-align: start;
   }
 `;
